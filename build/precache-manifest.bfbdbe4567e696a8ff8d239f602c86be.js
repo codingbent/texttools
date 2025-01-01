712f9057fd04845e396646b17946ddf5e5a7b90f
abhed agarwal <abhed.agl@gmail.com>
2025-01-02 02:10:54 +0530

    edit

---
+++ build/precache-manifest.bfbdbe4567e696a8ff8d239f602c86be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43ec4e70bb2d851705e0f4393e9a120d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fef6b05078771f7cfa63",
+    "url": "/static/css/main.ec0d66e0.chunk.css"
+  },
+  {
+    "revision": "517dac025caa32a38607",
+    "url": "/static/js/2.174f6a7a.chunk.js"
+  },
+  {
+    "revision": "b95080709ac12d0967db165d90e6c04a",
+    "url": "/static/js/2.174f6a7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6aa5ad4c3ba0c3e292a",
+    "url": "/static/js/3.26d1c32d.chunk.js"
+  },
+  {
+    "revision": "fef6b05078771f7cfa63",
+    "url": "/static/js/main.181ac4ea.chunk.js"
+  },
+  {
+    "revision": "9d717785d7c885935657",
+    "url": "/static/js/runtime-main.7411dac8.js"
+  }
+]);
